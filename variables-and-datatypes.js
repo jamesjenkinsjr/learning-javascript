fb5da0f1d09a510182c3ddeb049e3c1cf57198be
James Jenkins <jamesjenkinsjr@icloud.com>
2018-01-29 18:54:46 -0500

    Add README and variables-and-datatypes

---
+++ variables-and-datatypes.js
@@ -0,0 +1,13 @@
+// single line comment
+/**
+* multiple line
+* comment
+*/
+
+//Variables
+var name = "James"; //old skool JS (deprecated)
+const fruit = "Banana"; //constant binding (lock it in the cup)
+let veggie = "Carrot"; //similar to var but other differences...
+console.log("Name: ", name); //print thing
+console.log("Fruit: ", fruit);
+console.log("Veggie: ", veggie);
