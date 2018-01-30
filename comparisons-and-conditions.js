9c1a1f1e947c69a35e4a30b34564ca33e8400d20
James Jenkins <jamesjenkinsjr@icloud.com>
2018-01-30 18:24:43 -0500

    Working with if, else, and elseif

--- comparisons-and-conditions.js
+++ comparisons-and-conditions.js
@@ -18,3 +18,36 @@ console.log('aString < anotherString: ', aNumber < anotherNumber);
 console.log('aString > anotherString: ', aNumber > anotherNumber);
 console.log('aString >= anotherString: ', aNumber >= anotherNumber);
 console.log('aString <= anotherString: ', aNumber <= anotherNumber);
+
+//Conditional statements
+// if -> using it to run code based on answer to question
+//if - else -> run code based on answer to question, with fallback
+//if - else if - else -> many questions that determine code to run
+//Note: There is a switch statement as well
+
+//if
+if(3 === 3){
+    console.log('Orange Crush is delicious');
+}
+
+if('a' === 'b'){
+    console.log('this will never print');
+}
+
+//if else
+if ('grape' === 'banana'){
+    console.log('this will never happen');
+} else {
+    console.log('This is a backup plan');
+}
+
+//if elseif else
+//const someNumber = 100;
+const someNumber = 42;
+if(someNumber === 100){
+    console.log('it is 100');
+} else if(someNumber === 42){
+    console.log('The meaning of life');
+} else {
+    console.log('None of the others worked');
+}
