58228c6deb96be656f33c4a3bdc579b7b7ebd02e
James Jenkins <jamesjenkinsjr@icloud.com>
2018-01-29 19:35:34 -0500

    Go oover numbers and use typeof

--- variables-and-datatypes.js
+++ variables-and-datatypes.js
@@ -2,7 +2,7 @@
 /**
 * multiple line
 * comment
-*/
+**/
 
 //Variables
 var name = "James"; //old skool JS (deprecated)
@@ -11,3 +11,24 @@ let veggie = "Carrot"; //similar to var but other differences...
 console.log("Name: ", name); //print thing
 console.log("Fruit: ", fruit);
 console.log("Veggie: ", veggie);
+//Simple Data Types
+//Strings
+const aString = "sterf and thangz";
+const anotherString = 'other thangs and sterf';
+const stringWithNumbers = '1234.56';
+console.log("aString: ", aString);
+console.log("anotherString: ", anotherString);
+console.log("stringWithNumbers: ", stringWithNumbers);
+console.log("What is the type of 'aString'? ", typeof aString);
+
+//Numbers
+const aNumber = 1;
+const anotherNumber = 3.5;
+console.log("aNumber: ", aNumber);
+console.log("anotherNumber: ", anotherNumber);
+console.log("aNumber: ", typeof aNumber);
+console.log("anotherNumber: ", typeof anotherNumber);
+
+//Arrays
+const anArray = [];
+console.log("anArray: ", typeof anArray);
