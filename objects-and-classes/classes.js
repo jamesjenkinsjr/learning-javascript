96ccf5b7a1a03b968b8c6ce0b7b360d570c4006d
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-13 19:45:10 -0500

    Create truck class that demos the extends and super keywords, thus allowing a 'Truck' to be a 'Vehicle' that has unqiue props and methods on top of Vehicle

--- objects-and-classes/classes.js
+++ objects-and-classes/classes.js
@@ -3,12 +3,16 @@ class Vehicle {
         this.color = color;
         this.stank = stank;
     }
+    drive() {
+        console.log('VROOOOOM!');
+    }
 }
 
 const v = new Vehicle();
 const v2 = new Vehicle('yellow', 'old car smell');
 console.log(v);
 console.log(v2);
+v.drive();
 
 //Crea an object assigned to the variable 'vLiteral' using syntax
 //similar to the circle, but you are to make a vehicle.
@@ -22,3 +26,20 @@ const vLiteral = {
     stank: 'new car',
 };
 console.log(vLiteral);
+
+class Truck extends Vehicle {
+    constructor(color='red', stank='new stank', hasLiftKit=true) {
+        super(color, stank);
+        this.hasLiftKit = hasLiftKit;
+    }
+    goMudding() {
+        console.log('suuuuuuuuuuueeyyyy!');
+    }
+    drive() {
+        console.log('bwaabwabwabwabwah');
+    }
+}
+const t = new Truck();
+t.goMudding();
+t.drive();
+console.log(t);
